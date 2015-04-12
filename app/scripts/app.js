c90551a365880b896e37daddfe2b00480ec53347
Imran Khoja <imrankhoja1@gmail.com>
2015-04-12 08:09:39 -0400

    work on modal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,51 @@
 var app = angular.module('BlocChat', [
   'firebase',
-  'ui.router'
+  'ui.router',
+  'ui.bootstrap'
   ])
 
   app.controller('HomeController', ['$scope', 'Room', function($scope, Room) {
-    console.log("Hello guys, I figured it out!");
 
     // get list of rooms from factory
     $scope.rooms = Room.all;
 
     $scope.createRoom = function() {
       // trigger modal
+      $scope.open();
       Room.add();
     }
   }]);
-/*
-  app.service('Room', function($rootScope, $firebaseArray) {
-    var ref = new Firebase("https://bloc-chat1.firebaseio.com/");
-  })
-*/
+
+  app.controller('ModalController', function($scope, $modal){
+    console.log("Hello guys, I figured it out!");
+
+    $scope.items = ["item1"];
+    $scope.open = function() {
+
+      var modalInstance = $modal.open({
+        
+        templateUrl: 'templates/new-chat.html',
+        controller: 'ModalController',
+        resolve: {
+          items: function() {
+            return $scope.items;
+          }
+        }
+      });
+
+      $scope.ok = function() {
+        $modalInstance.close();
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+      };
+  }
+  });
 
   app.factory('Room', ['$firebaseArray', function($firebaseArray) {
     var firebaseRef = new Firebase("https://bloc-chat1.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef);
-
-    // // create a new chatroom
-    // rooms.$add({ foo: "bar" }).then(function(firebaseRef) {
-    //   var id = firebaseRef.key();
-    //   console.log("Added record with id " + id);
-    //   rooms.$indexFor(id);
-    // });
 
     return {
       all: rooms,
