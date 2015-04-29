270fe3154fb62e98373b1dec7080aa5edab7d4d3
Imran Khoja <imrankhoja1@gmail.com>
2015-04-28 20:22:24 -0400

    username now sets, just figure out how to trigger it when you create a new room

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,12 +6,22 @@ var app = angular.module('BlocChat', [
 
   app.controller('HomeController', ['$scope', 'Room', function($scope, Room) {
 
+    var currentRoom = null;
     // get list of rooms from factory
     $scope.rooms = Room.all;
+    debugger;
 
+    $scope.selectRoom = function(roomId) {
+      currentRoom = Room.getRoomById(roomId);
+      parseMessages(currentRoom);
+    }
+
+    parseMessages = function(currentRoom) {
+      return currentRoom.val();
+    }
   }]);
 
-// create two of these to allow for username.
+// first modal for NewRoom.
 app.controller('ModalCtrl', ['$scope', '$modal', '$log', 'Room', function($scope, $modal, $log, Room) {
 
 
@@ -42,12 +52,42 @@ app.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'Room', functio
     };
 }]);
 
-  app.factory('Room', ['$firebaseArray', function($firebaseArray) {
+// second modal for Username.
+app.controller('UserCtrl', ['$scope', '$modal', '$log', 'Room', function($scope, $modal, $log, Room) {
+
+
+    $scope.setUsername = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: '/templates/set-username.html',
+            controller: 'UserInstanceCtrl',
+            size: size
+        });
+
+        modalInstance.result.then(function (username) { 
+          Room.setUsername(username);
+        }, function () {
+          $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+}]);
+
+app.controller('UserInstanceCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+
+    $scope.ok = function (username) {
+      $modalInstance.close(username);
+    };
+}]);
+
+  app.service('Room', ['$firebaseArray', function($firebaseArray) {
     var firebaseRef = new Firebase("https://bloc-chat1.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef);
+    var currentRoom = rooms.first;
+    var user = null;
 
     return {
       all: rooms,
+      currentRoom: currentRoom,
 
       add: function(roomName) {
         // create a new chatroom
@@ -55,10 +95,16 @@ app.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', 'Room', functio
         rooms.$add({ displayName: roomName }).then(function(firebaseRef) {
         var id = firebaseRef.key();
         console.log("Added record with id " + id);
+        // seems like you'd ideally want a prompt here to capture the username
         });
       },
-      messages: function(roomId) {
-        return rooms.orderByChild(roomId);
+      getRoomById: function(roomId) {
+        console.log("https://bloc-chat1.firebaseio.com/" + roomId);
+        return new Firebase("https://bloc-chat1.firebaseio.com/" + roomId).child("messages").on("value", function(messages) { window.messages = messages });
+      },
+      setUsername: function(username) {
+        user = username;
+        console.log(user);
       }
     }
   }])
